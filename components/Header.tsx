4e0b670fe88f2314a7f53833ea019ea27168ed86
Aurino Geraldo <aurinoaj@gmail.com>
2026-02-17 22:47:11 -0300

    chore: add archived page and update header layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
-import { ShoppingBag } from "lucide-react";
+import { Archive, ShoppingBag } from "lucide-react";
+import Link from "next/link";
 
 export const Header = () => {
 	return (
@@ -8,14 +9,19 @@ export const Header = () => {
 			animate={{ opacity: 1, y: 0 }}
 			className="mb-8"
 		>
-			<div className="flex items-center gap-3 mb-2">
-				<ShoppingBag size={32} className="text-orange-400" />
-				<h1 className="text-3xl font-bold text-neutral-800">
-					Lista de Compras
-				</h1>
+			<div className="flex justify-between items-center mb-4">
+				<div className="flex items-center gap-3">
+					<ShoppingBag size={32} className="text-orange-400" />
+					<h1 className="text-3xl font-bold text-neutral-800">Duolist</h1>
+				</div>
+				<Link href="/arquivados">
+					<div className="w-8 h-8 bg-orange-400 rounded-xl flex items-center justify-center">
+						<Archive size={18} className="text-orange-100" />
+					</div>
+				</Link>
 			</div>
-			<p className="text-neutral-600 ml-11">
-				Organize suas compras de forma simples e eficiente
+			<p className="text-neutral-600">
+				Organizando as compras, pra não voltar só com besteira.
 			</p>
 		</motion.header>
 	);
